a6218107e4dd103153d8c4ac2483dfec61522746
pinusc <giuseppe.stelluto@gmail.com>
2015-08-25 17:20:51 +0200

    Real initial commit

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import render_template
+import subprocess
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	try:
+		fortune = subprocess.check_output('fortune -s', shell=True, universal_newlines=True)
+		cow = subprocess.check_output('echo "%s" | cowsay -f $(ls /usr/share/cows|  shuf -n1)' % (fortune), shell=True, universal_newlines=True)
+	except Exception as e:
+		print(e)
+	return render_template('home.html', cowsay=cow)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
