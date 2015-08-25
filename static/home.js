0a4e9f9ce318b5ae3f2dcc6d6b03849cbd815dc7
pinusc <giuseppe.stelluto@gmail.com>
2015-08-25 23:09:53 +0200

    Add zero-padding to time

--- static/home.js
+++ static/home.js
@@ -1,4 +1,10 @@
 function main(){
 	var currentDate = new Date();
-	document.getElementById("clock").innerHTML = currentDate.getHours() + ':' + currentDate.getMinutes();
+	var minutes = currentDate.getMinutes();
+	if (minutes < 10)
+		minutes = '0' + String(minutes)
+	var hours = currentDate.getHours();
+	if (hours < 10)
+		hours = '0' + String(hours)
+	document.getElementById("clock").innerHTML = hours + ':' + minutes;
 }
